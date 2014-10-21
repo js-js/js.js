52b883e1fcda8257d60e859265ee188062b17d44
Fedor Indutny <fedor@indutny.com>
2014-10-21 21:43:24 +0400

    compiler: refactor global loads/stores

--- test/api-test.js
+++ test/api-test.js
@@ -9,39 +9,54 @@ describe('js.js API', function() {
     r = js.create();
   });
 
-  it('should do basic binary expression', function() {
-    var fn = r.compile('(1 + 2) + (3 + 4)');
-    r.heap.gc();
-    assert.equal(fn.call(null, []).cast().value(), 10);
+  describe('basics', function() {
+    it('should do basic binary expression', function() {
+      var fn = r.compile('(1 + 2) + (3 + 4)');
+      r.heap.gc();
+      assert.equal(fn.call(null, []).cast().value(), 10);
+    });
+
+    it('should do string literal', function() {
+      var fn = r.compile('"okish"');
+      r.heap.gc();
+      assert.equal(fn.call(null, []).cast().toString(), 'okish');
+    });
   });
 
-  it('should do string literal', function() {
-    var fn = r.compile('"okish"');
-    r.heap.gc();
-    assert.equal(fn.call(null, []).cast().toString(), 'okish');
+  describe('global', function() {
+    it('should do global fetch', function() {
+      var global = r.heap.context.global().cast();
+      global.set(r.heap.allocString('ohai'), r.heap.allocString('ok'));
+
+      var fn = r.compile('ohai');
+      r.heap.gc();
+      var res = fn.call(null, []).cast();
+      assert.equal(res.toString(), 'ok');
+    });
+
+    it('should do global store', function() {
+      var fn = r.compile('ohai = "oook";ohai');
+      r.heap.gc();
+      var res = fn.call(null, []).cast();
+      assert.equal(res.toString(), 'oook');
+    });
+
+    it('should do math with globals', function() {
+      var fn = r.compile('var a = 1, b = 2, c = 3; (a + c)  + (b + c)');
+      r.heap.gc();
+      var res = fn.call(null, []).cast();
+      assert.equal(res.value(), 9);
+    });
   });
 
-  it('should do global fetch', function() {
-    var global = r.heap.context.global().cast();
-    global.set(r.heap.allocString('ohai'), r.heap.allocString('ok'));
-
-    var fn = r.compile('ohai');
-    r.heap.gc();
-    var res = fn.call(null, []).cast();
-    assert.equal(res.toString(), 'ok');
-  });
-
-  it('should do global store', function() {
-    var fn = r.compile('ohai = "oook";ohai');
-    r.heap.gc();
-    var res = fn.call(null, []).cast();
-    assert.equal(res.toString(), 'oook');
-  });
-
-  it('should do math with globals', function() {
-    var fn = r.compile('var a = 1, b = 2, c = 3; (a + c)  + (b + c)');
-    r.heap.gc();
-    var res = fn.call(null, []).cast();
-    assert.equal(res.value(), 9);
+  // TODO(indutny): enable after allocation will be implemented
+  return;
+  describe('allocator', function() {
+    it('should allocate object', function() {
+      var fn = r.compile('var a = {};a.x = "ok";a.x');
+      r.heap.gc();
+      var res = fn.call(null, []).cast();
+      assert.equal(res.toString(), 'ok');
+    });
   });
 });
